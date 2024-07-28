bab42875e26b1d75fc70bd7b9205aeb14cfc678a
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-07-28 19:25:32 +0800

    ok

--- Project/Link/Cable.hpp
+++ Project/Link/Cable.hpp
@@ -15,8 +15,6 @@ class Cable: public ParticleLink
 
 		Vector3 anchorPosition;
 
-		bool isStiff = false;
-
 		inline Cable(Vector3 anchorPosition, Particle* p, float cableLength): anchorPosition(anchorPosition), cableLength(cableLength){
 			particles[0] = new Particle(); 
 			particles[0]->position = anchorPosition;
